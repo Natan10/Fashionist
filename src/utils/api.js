711efdd7fcc91ad4275daab7ff235be22cc5da9c
Natan <natan-moreira1@hotmail.com>
2020-07-07 17:35:09 -0300

    api codenation

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,8 @@
 // Pegando dados da Api
-const URL = 'https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog'
+//const URL = 'https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog'
+
+const URL = 'http://localhost:4000/products'
+
 
 export default function api(){
   return fetch(URL).
